004d904fce3073412bc1082f04b0247405cc80c1
Vikas Gavandi <30316401+DIGILABTEAM@users.noreply.github.com>
2021-11-22 11:10:58 +0530

    deployment

---
+++ README.md
@@ -0,0 +1,3 @@
+<p>Developed By Digilab</p>
+# GP-QUIZ 
+Developed By Digilab <a href="https://digilabteam.github.io/CETANILQUIZ1/">Cetanil TM GAME </a>
---
+++ js/questions.js
@@ -0,0 +1,49 @@
+/*All questions in the quiz goes here in the data object.....*/
+var data =
+	[
+		[
+			{
+				question: "Cilnidipine",
+				option1: "Reduces PVR via inhibition of L-/N-type calcium channel in the arteries.",
+				option2: "RAAS suppression.",
+				option3: "Decrease  CO-decrease heart rate & force of contraction.",
+				answer: 1
+			},
+			{
+				question: "Telmisartan",
+				option1: "Reduces PVR via inhibition of L-/N-type calcium channel in the arteries.",
+				option2: "RAAS suppression.",
+				option3: "Decrease  CO-decrease heart rate & force of contraction.",
+				answer: 2
+			},
+			{
+				question: "Metoprolol",
+				option1: "RAAS suppression.",
+				option2: "Decrease CO-decrease heart rate & force of contraction.",
+				option3: "Reduces PVR via inhibition of L-/N-type calcium channel in the arteries.",
+				answer: 2
+			},
+			{
+				question: "Cetanil-TM Indication",
+				option1: "Superior 24 - hour blood pressure control",
+				option2: "One tablet, once in a day",
+				option3: "Treatment of patients with uncontrolled hypertension.",
+				answer: 3
+			},
+			{
+				question: "Cetanil-TM Dosage",
+				option1: "Treatment of patients with uncontrolled hypertension",
+				option2: "One tablet, once in a day",
+				option3: "Superior 24 - hour blood pressure control",
+				answer: 2
+			},
+			{
+				question: "CETANIL TM  Advantage",
+				option1: "One tablet, once in a day.",
+				option2: "Superior 24 - hour blood pressure control.",
+				option3: "Treatment of patients with uncontrolled hypertension.",
+				answer: 2
+			}
+
+		]
+	];
